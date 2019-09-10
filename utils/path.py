2cbc0c383794f29f6d5beb13fd26b2d528bde490
Henrik Petersson <henrik@tutamail.com>
2019-09-10 20:14:40 +0200

    Check file type with magic

--- utils/path.py
+++ utils/path.py
@@ -1,12 +1,14 @@
+import csv
 import io
 import os
-import csv
 
+import magic
 from PIL import Image
 
 
 def _check_path(output):
     """Create folder for downloaded files if it not exist."""
+
     if not os.path.isdir(output):
         os.makedirs(output)
 
@@ -15,8 +17,10 @@ def save_meta(data, output):
     """Save post metadata in a csv file."""
 
     _check_path(output)
+
     # Path to the output file.
     metadata = os.path.join(output, "metadata.csv")
+
     # Inner function that writes data to csv.
     def dump(data, path=""):
         for i in data.keys():
@@ -29,7 +33,10 @@ def save_meta(data, output):
             else:
                 with open(metadata, mode="a") as f:
                     dumpe_writer = csv.writer(
-                        f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+                        f,
+                        delimiter=",",
+                        quotechar='"',
+                        quoting=csv.QUOTE_MINIMAL,
                     )
                     dumpe_writer.writerow([path + f"[{key}]", value])
 
@@ -50,20 +57,19 @@ def save_file(buffer, output, filename):
     """
 
     _check_path(output)
-    # JPEG file signature always start with FF D8.
-    # The other two bytes are FF Ex (x = 0-F).
-    if buffer[:3] == b"\xff\xd8\xff" and (buffer[3] & 0xE0) == 0xE0:
+
+    # Look at the first 12 bytes to determine the file type,
+    # because the first 4 bytes are needed for JPEGs and MP4 signatures
+    # are 8 bytes long and are offset by 4 bytes, which means 12 bytes are
+    # a minimum requirement in those cases.
+    file_type = magic.from_buffer(buffer[:12], mime=True)
+
+    if file_type == "image/jpeg":
         bytes = io.BytesIO(buffer)
         op = os.path.join(output, filename)
         # Save the image with Pillow to remove any unwanted meta data.
         # Also try to keep the same quality when saved.
         Image.open(bytes).save(op, quality="keep")
-    # MP4 file signatures are 8 bytes long and are offset by 4 bytes.
-    # The first four bytes are the same in both types of MP4 file formats.
-    elif (
-        buffer[4:8] == b"\x66\x74\x79\x70"
-        and buffer[8:12] == b"\x69\x73\x6f\x6d"
-        or buffer[8:12] == b"\x4d\x53\x4e\x56"
-    ):
+    elif file_type == "video/mp4":
         with open(os.path.join(output, filename), "wb") as f:
             f.write(buffer)
