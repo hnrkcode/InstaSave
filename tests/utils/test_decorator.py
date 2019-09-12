fb8f17a28cc56b7ed084f059b3184750b848dbbb
Henrik Petersson <henrik@tutamail.com>
2019-09-12 16:11:56 +0200

    Refactor decorator tests

--- tests/utils/test_decorator.py
+++ tests/utils/test_decorator.py
@@ -7,7 +7,23 @@ from utils import decorator
 
 class TestDecorators(unittest.TestCase):
     def setUp(self):
-        pass
+        class Cls:
+            def __init__(self, verbose):
+                self.verbose = verbose
+
+            @decorator.count_calls
+            def func(self):
+                pass
+
+        def call_prints(calls, verbose):
+            captured = io.StringIO()
+            sys.stdout = captured
+            for i in range(calls):
+                Cls(verbose).func()
+            sys.stdout = sys.__stdout__
+            return captured.getvalue()
+
+        self.output = call_prints
 
     def test_unique_filename(self):
         def func():
@@ -17,49 +33,13 @@ class TestDecorators(unittest.TestCase):
         self.assertRegex(filename(), "^generic_filename_[0-9a-z]{20}.jpg$")
 
     def test_count_calls_verbose_off(self):
-        class Cls:
-            def __init__(self, verbose):
-                self.verbose = verbose
-
-            @decorator.count_calls
-            def func(self):
-                pass
-
-        def call_prints(calls, verbose):
-            capturedOutput = io.StringIO()
-            sys.stdout = capturedOutput
-            for i in range(calls):
-                Cls(verbose).func()
-            sys.stdout = sys.__stdout__
-
-            return capturedOutput.getvalue()
-
         calls = 5
         print_output = "." * calls
-
-        self.assertEqual(call_prints(calls, False), print_output)
+        self.assertEqual(self.output(calls, False), print_output)
 
     def test_count_calls_verbose_on(self):
-        class Cls:
-            def __init__(self, verbose):
-                self.verbose = verbose
-
-            @decorator.count_calls
-            def func(self):
-                pass
-
-        def call_prints(calls, verbose):
-            capturedOutput = io.StringIO()
-            sys.stdout = capturedOutput
-            for i in range(calls):
-                Cls(verbose).func()
-            sys.stdout = sys.__stdout__
-
-            return capturedOutput.getvalue()
-
         calls = 5
         print_output = "".join(
             [f"Post {post}\n" for post in range(1, calls + 1)]
         )
-
-        self.assertEqual(call_prints(calls, True), print_output)
+        self.assertEqual(self.output(calls, True), print_output)
